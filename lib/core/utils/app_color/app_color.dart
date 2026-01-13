2569fd5b99fbd053fa94218657949598053c3eb4
Ahmed Medhat <131293372+AhmedMedhatHamed@users.noreply.github.com>
2026-01-13 13:03:20 +0200

    strings and colors done

---
+++ lib/core/utils/app_color/app_color.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+abstract class AppColor {
+  static const Color primaryColor = Colors.orange;
+  static const Color deepOrange = Colors.deepOrange;
+  static const Color offWhite = Color(0xffF8F4F9);
+  static const Color boldStrings = Color(0xff333333);
+  static const Color strings = Color(0xff6F6460);
+}
---
+++ lib/core/utils/app_strings/app_strings.dart
@@ -0,0 +1,10 @@
+abstract class AppStrings {
+  static const String onBoarding1 = 'assets/images/onBoarding1.png';
+  static const String onBoarding2 = 'assets/images/onBoarding2.png';
+  static const String onBoarding3 = 'assets/images/onBoarding3.png';
+  static const String successOrder = 'assets/images/Succces order icon.png';
+  static const String forgetPassword = 'assets/images/forget password.png';
+  static const String vector2 = 'assets/images/Vector(1).svg';
+  static const String vector1 = 'assets/images/Vector.svg';
+  static const String appName = 'Artefacta';
+}
